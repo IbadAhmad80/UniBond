14664271df7676116d0a407318652df83087eb8b
UniBond12 <uniBond12@gmail.com>
2022-08-13 19:24:49 +0500

    make it better

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         apiUrl:
             process.env.NODE_ENV === "development"
                 ? "http://localhost:3000" // development api
-                : "https://bond-uni.herokuapp.com/", // production api
+                : "https://uni-bond-app.herokuapp.com/", // production api
     },
     serverRuntimeConfig: {
         secret: "THIS IS USED TO SIGN AND VERIFY JWT TOKENS, IT CAN BE ANY STRING",
